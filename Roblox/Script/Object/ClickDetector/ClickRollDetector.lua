25c9471f48c39031ca512487c80f3cab026b3456
EditaNEmilis <ramanauskiene.edita@gmail.com>
2023-02-21 16:20:21 +0200

    ClickRoll fix
    Okay.. Maybe not once, can be a Roblox Studio bug by chance.

--- Roblox/Script/Object/ClickDetector/ClickRollDetector.lua
+++ Roblox/Script/Object/ClickDetector/ClickRollDetector.lua
@@ -1,6 +1,6 @@
 local clickdetector = script.Parent.ClickDetector
 local roll = math.random(1,6)
 
-clickdetector.MouseClick:Once(function() -- Sometimes the local roll only keep the same number and not the other number, only set it to Once.
+clickdetector.MouseClick:Connect(function()
 	print("You got a" ,(roll) "!")
 end)
