c8bef128989c4e408904490a50c6ea0d4ba620ca
EditaNEmilis <ramanauskiene.edita@gmail.com>
2023-03-12 00:09:38 +0200

    Raycasting script
    Added script for Roblox raycasting function..

---
+++ Roblox/Script/Part/Raycast/Raycasting.lua
@@ -0,0 +1,11 @@
+-- Get Locals
+local caster = workspace.Caster
+local target = workspace.Target
+
+local direction = caster.Position - target.Position -- Set direction
+
+local raycast = workspace:Raycast(target.Position, direction) -- Raycasting
+
+if raycast and raycast.Instance then
+	raycast.Instance.Color = Color3.new(1, 0.666667, 0) -- Set Color
+end
