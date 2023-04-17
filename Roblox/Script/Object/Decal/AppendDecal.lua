825f1e0ce4dfaff554c039d47bcbd26c689ba882
EditaNEmilis <107065286+EditaNEmilis@users.noreply.github.com>
2023-04-17 22:17:40 +0300

    Object Decal scripts
    Appending and Randomness.. Easy and simple!

---
+++ Roblox/Script/Object/Decal/AppendDecal.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local decal = brick.Decal
+
+decal.Texture = "http://www.roblox.com/asset/?id=".. 3203186317 ..""
---
+++ Roblox/Script/Object/Decal/RandomDecal.lua
@@ -0,0 +1,5 @@
+local brick = script.Parent
+local decal = brick.Decal
+local random = math.random(1, 1000000000)
+
+decal.Texture = "http://www.roblox.com/asset/?id=".. (random) ..""
