ccee2b131f90b8586563fea09b1f9a03c8da3efb
EditaNEmilis <ramanauskiene.edita@gmail.com>
2023-04-23 23:42:25 +0300

    Rain Part script
    Let it rain to the ground!..

---
+++ Roblox/Script/ServerScriptService/RainPart.lua
@@ -0,0 +1,14 @@
+while wait(0.1) do
+	local part = Instance.new("Part")
+	part.Position = Vector3.new(math.random(1, 100), 30, math.random(1, 100))
+	part.Size = Vector3.new(0.5, 3, 0.5)
+	part.Parent = game.Workspace
+	
+	part.Touched:Connect(function(touch)
+		touch = game.Workspace.Baseplate
+		if touch then
+			wait(0.2) -- Small delay before destorying it
+			part:Destroy()
+		end
+	end)
+end
