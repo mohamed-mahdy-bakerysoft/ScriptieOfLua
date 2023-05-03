0f5368e430dc32a09fe9a9c404f2d6e324642caf
EditaNEmilis <107065286+EditaNEmilis@users.noreply.github.com>
2023-05-03 17:14:11 +0300

    Scripts Updated
    ElevatorPrompt script tuning.
    Humanoid WalkSpeed and JumpPower touched script has less line.
    And new folders for touched.

--- Roblox/Script/Object/ProximityPrompt/ElevatorPrompt.lua
+++ Roblox/Script/Object/ProximityPrompt/ElevatorPrompt.lua
@@ -3,30 +3,24 @@ local brick = script.Parent
 local button = brick.ProximityPrompt
 
 -- Function
--- Go Up
 button.Triggered:Connect(function()
-	repeat
-		button.Enabled = false
-		wait(0.1)
-		brick.Position = brick.Position + Vector3.new(0, 0.5, 0)
-	until brick.Position.Y >= 10
-	
-	if brick.Position.Y >= 10 then
-		button.Enabled = true
-	end
-end)
-
--- Go Down
-button.Triggered:Connect(function()
-	if brick.Position.Y >= 10 then
-		repeat 
+	if brick.Position.Y <= 10 then -- Go Up
+		repeat
+			button.Enabled = false
+			wait(0.1)
+			brick.Position = brick.Position + Vector3.new(0, 0.5, 0)
+		until brick.Position.Y >= 10
+	elseif brick.Position.Y >= 10 then -- Go Down
+		repeat
 			button.Enabled = false
 			wait(0.1)
 			brick.Position = brick.Position + Vector3.new(0, -0.5, 0)
 		until brick.Position.Y <= 0.5
-		
-		if brick.Position.Y <= 0.5 then
-			button.Enabled = true
-		end
+	end
+
+	if brick.Position.Y >= 10 then
+		button.Enabled = true
+	elseif brick.Position.Y <= 0.5 then
+		button.Enabled = true
 	end
 end)
--- Roblox/Script/Part/Touched/Humanoid/HumanoidWalkSpeed.lua
+++ Roblox/Script/Part/Touched/Humanoid/HumanoidWalkSpeed.lua
@@ -1,14 +1,6 @@
 local button = script.Parent
 
 button.Touched:Connect(function(touch)
-	if not touch.Parent then
-		return
-	end
-	
 	local humanoid = touch.Parent:FindFirstChild("Humanoid")
-	if not humanoid then
-		return
-	end
-	
-	humanoid.JumpPower = 100
+	humanoid.WalkSpeed = 35 -- Set Player speed
 end)
