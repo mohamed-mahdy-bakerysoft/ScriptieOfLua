5dcc24622254448e89107a5326d808fe7fb1c2cb
EditaNEmilis <ramanauskiene.edita@gmail.com>
2023-05-09 18:39:49 +0300

    Mouse scripts
    Appending Mouse Icon andddd.. Printing. Don't ask.

---
+++ Roblox/LocalScript/Console/MouseLocation.lua
@@ -0,0 +1,6 @@
+local player = game.Players.LocalPlayer
+local mouse = player:GetMouse()
+
+while wait(1) do
+	print(mouse.X, mouse.Y)
+end
---
+++ Roblox/LocalScript/StarterPlayer/StarterCharacterScripts/Mouse/AppendMouseIcon.lua
@@ -0,0 +1,4 @@
+local player = game.Players.LocalPlayer
+local mouse = player:GetMouse()
+
+mouse.Icon = "http://www.roblox.com/asset/?id=6549216050"
