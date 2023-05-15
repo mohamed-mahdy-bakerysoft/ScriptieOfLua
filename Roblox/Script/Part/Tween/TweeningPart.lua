979b494f9a724708b1e24a02bdb2a732c4512790
EditaNEmilis <ramanauskiene.edita@gmail.com>
2023-05-15 22:36:40 +0300

    Tweening Part script
    If you want to make your part move smooth or quick.. Tweening is the thing!

---
+++ Roblox/Script/Part/Tween/TweeningPart.lua
@@ -0,0 +1,16 @@
+local tweenservice = game:GetService("TweenService") -- Get service
+local part = script.Parent
+
+-- Create an TweenInfo
+local info = TweenInfo.new(
+	3, -- Time
+	Enum.EasingStyle.Linear, -- Easing interpolation
+	Enum.EasingDirection.Out, -- Direction
+	-1, -- Repeat loop (Setting to -1 will make it forever)
+	true -- Reversing
+)
+
+local newpos = part.Position + Vector3.new(0, 5, 0) -- New position
+local tween = tweenservice:Create(part, info, {Position = newpos}) -- Tween
+
+tween:Play() -- Play
