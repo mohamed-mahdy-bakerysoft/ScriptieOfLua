a6fbf5e4ed3280c796892c92f86ce792978af1e5
EditaNEmilis <107065286+EditaNEmilis@users.noreply.github.com>
2023-05-25 19:04:11 +0300

    Set Scale scripts
    Tried to use Activate().. But didn't work, probably might tune it later..

---
+++ GarrysMod/Script/Entities/EntitySetScale.lua
@@ -0,0 +1,3 @@
+for _, ent in pairs(ents.FindByClass("prop_physics")) do
+    ent:SetModelScale(5, 5) -- Size, DeltaTime
+end
---
+++ GarrysMod/Script/Player/PlayerSetScale.lua
@@ -0,0 +1,3 @@
+for _, ply in pairs(player.GetAll()) do
+    ply:SetModelScale(5, 5) -- Size, DeltaTime
+end
