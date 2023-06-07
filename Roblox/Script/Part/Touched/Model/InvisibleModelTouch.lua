d5111318d650a933d3ee23c19ba38d2197ff5b8e
EditaNEmilis <107065286+EditaNEmilis@users.noreply.github.com>
2023-06-07 18:19:43 +0300

    Minor updates
    Updated Invisible Model Touch script and moved some scripts with new folder type.

--- Roblox/Script/Part/Touched/Model/InvisibleModelTouch.lua
+++ Roblox/Script/Part/Touched/Model/InvisibleModelTouch.lua
@@ -2,15 +2,18 @@ local button = script.Parent
 
 button.Touched:Connect(function(hit)
 	local hide = hit.Parent:GetChildren()
-	
+
 	for i = 1, #hide do
 		-- Make the MeshPart invisible
 		if (hide[i].ClassName == "MeshPart") then
+			if hide[i].Name == "Head" then
+				hide[i].face.Transparency = 1
+			end
 			hide[i].Transparency = 1
-			
+
 		-- Remove any accessories
 		elseif (hide[i].ClassName == "Accessory") then
-			hide[i]:Remove()
+				hide[i].Handle.Transparency = 1
 		end
 	end
 end)
