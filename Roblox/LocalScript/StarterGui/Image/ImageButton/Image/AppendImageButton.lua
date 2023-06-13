0fbaeb3faad452031bcb7c9bdada6a733e14eca5
EditaNEmilis <107065286+EditaNEmilis@users.noreply.github.com>
2023-06-13 17:41:15 +0300

    StarterGui append image scripts
    Both for ImageButton and ImageLabel.

---
+++ Roblox/LocalScript/StarterGui/Image/ImageButton/Image/AppendImageButton.lua
@@ -0,0 +1,5 @@
+local imagebutton = script.Parent.ImageButton
+
+imagebutton.MouseButton1Click:Connect(function()
+	imagebutton.Image = "http://www.roblox.com/asset/?id=3203186317"
+end)
---
+++ Roblox/LocalScript/StarterGui/Image/ImageLabel/Image/AppendImageLabel.lua
@@ -0,0 +1,3 @@
+local imagelabel = script.Parent.ImageLabel
+
+imagelabel.Image = "http://www.roblox.com/asset/?id=3203186317"
