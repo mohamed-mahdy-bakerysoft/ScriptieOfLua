4ab63bdcefb0bbf582cce4ee022776543f243540
EditaNEmilis <ramanauskiene.edita@gmail.com>
2023-07-11 17:43:14 +0300

    Play Sound script
    A coin sound when touched.. But nothing special.

---
+++ Roblox/Script/Part/Other/Touched/Object/Sound/PlaySound.lua
@@ -0,0 +1,10 @@
+local button = script.Parent
+local sound = button.Sound
+
+button.Touched:Connect(function(hit)
+	local humanoid = hit.Parent:FindFirstChild("Humanoid")
+	if humanoid then
+		sound.SoundId = "rbxassetid://1453109725"
+		sound:Play()
+	end
+end)
