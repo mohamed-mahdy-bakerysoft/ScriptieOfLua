74ab183e944f18ab9569de6e16c6c4c2b7700d55
EditaNEmilis <ramanauskiene.edita@gmail.com>
2023-07-23 16:31:31 +0300

    Guesser Chat script
    The key is 'Hello World'.

---
+++ Roblox/Script/Part/Other/Players/GuesserChat.lua
@@ -0,0 +1,25 @@
+-- Locals
+local brick = script.Parent
+local players = game.Players
+
+local key = brick.Key
+
+-- Chatted Function
+function Chatted(message, recipient, speaker)
+	local text = string.lower(speaker.Name)
+	message = string.lower(message)
+	if (message == string.lower(key.Value)) then
+		brick.CanCollide = false
+		brick.Transparency = 0.5
+		wait(5) -- Delay closing
+		brick.CanCollide = true
+		brick.Transparency = 0
+	end 
+end 
+
+-- Connect Function
+players.ChildAdded:Connect(function(plr)
+	plr.Chatted:Connect(function(message, recipient)
+		Chatted(message, recipient, plr) 
+	end) 
+end)
