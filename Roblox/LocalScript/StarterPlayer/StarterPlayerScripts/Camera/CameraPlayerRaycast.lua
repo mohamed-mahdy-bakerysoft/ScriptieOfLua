c1ccf6611d9b3a62da0518ba7ac5d56269b11925
EditaNEmilis <ramanauskiene.edita@gmail.com>
2023-08-03 19:23:29 +0300

    Camera Player Raycast script
    Only detects the baseplate.. Maybe.

---
+++ Roblox/LocalScript/StarterPlayer/StarterPlayerScripts/Camera/CameraPlayerRaycast.lua
@@ -0,0 +1,29 @@
+-- Locals
+local players = game.Players.LocalPlayer
+
+local camera = workspace.CurrentCamera
+local start = camera.CFrame.Position
+
+local range = 10
+
+while wait(1) do 
+	-- Direction
+	local direction = camera.CFrame.LookVector * range
+	
+	-- Fitler Raycast
+	local params = RaycastParams.new()
+	params.FilterType = Enum.RaycastFilterType.Exclude
+	if players and players.Character then
+		params.FilterDescendantsInstances = {players.Character}
+	else
+		params.FilterDescendantsInstances = {nil}
+	end
+	
+	-- Get result
+	local result = workspace:Raycast(start, direction, params)
+	if result and result.Instance:IsA("Part") then
+		print("Looking at", result.Instance.Name .."..")
+	else
+		print("Looking at nothing..")
+	end
+end
