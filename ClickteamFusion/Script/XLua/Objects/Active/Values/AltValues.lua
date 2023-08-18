cbc5d588e8f9ee32f96355a01931a8997e1fc12e
EditaNEmilis <ramanauskiene.edita@gmail.com>
2023-08-18 15:52:10 +0300

    Alterable Values script
    For checking values. And a new folder tho.

---
+++ ClickteamFusion/Script/XLua/Objects/Active/Values/AltValues.lua
@@ -0,0 +1,10 @@
+function Values(id)
+    local object = mmf.newObject(id)
+
+	-- Index
+	local A = object.values[1]
+	-- MMF alphabetical letter
+	local B = object.values[mmf.ALT_B]
+	-- Name
+	local C = object.values['Value C']
+end
