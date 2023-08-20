e5f4bbc18e67f762d24972da1ff1b2cf32655322
EditaNEmilis <ramanauskiene.edita@gmail.com>
2023-08-20 22:51:20 +0300

    Part Color3 scripts
    Alternate to Brickcolor.

---
+++ Roblox/Script/Part/Apperance/Color/Color3/Color3.lua
@@ -0,0 +1,3 @@
+local brick = script.Parent
+
+brick.Color = Color3.new(0.752941, 1, 0.431373)
---
+++ Roblox/Script/Part/Apperance/Color/Color3/RandomColor3.lua
@@ -0,0 +1,5 @@
+local brick = script.Parent
+
+while wait(1) do
+	brick.Color = Color3.new(math.random(0.000, 1.000), math.random(0.000, 1.000), math.random(0.000, 1.000))
+end
