ddd394c6377d9740f875dc8ba33ee0b2e90f8feb
EditaNEmilis <ramanauskiene.edita@gmail.com>
2023-09-11 13:49:30 +0300

    Part Instance Scripts
    For parenting an object.

---
+++ Roblox/Script/Part/Other/Instance/InstanceFire.lua
@@ -0,0 +1,6 @@
+local brick = script.Parent
+local fire = Instance.new("Fire")
+
+fire.Size = 10
+fire.TimeScale = 0.5
+fire.Parent = brick
---
+++ Roblox/Script/Part/Other/Instance/InstanceHighlight.lua
@@ -0,0 +1,7 @@
+local brick = script.Parent
+local highlight = Instance.new("Highlight")
+
+highlight.DepthMode = "Occluded"
+highlight.FillTransparency = 1
+highlight.OutlineTransparency = 0.2
+highlight.Parent = brick
