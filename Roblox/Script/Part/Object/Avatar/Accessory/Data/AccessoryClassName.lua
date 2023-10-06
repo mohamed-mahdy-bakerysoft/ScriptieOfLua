663bcf1212a987659b477033e498a05176d0d5c8
EditaNEmilis <ramanauskiene.edita@gmail.com>
2023-10-06 22:12:33 +0300

    Object Avatar Accessory scripts
    Bit more than ForceField one. Maybe.

---
+++ Roblox/Script/Part/Object/Avatar/Accessory/Data/AccessoryClassName.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local accessory = brick.Accessory
+
+accessory.ClassName = "Accessory"
---
+++ Roblox/Script/Part/Object/Avatar/Accessory/Transform/AccessoryAttachmentPoint.lua
@@ -0,0 +1,5 @@
+local brick = script.Parent
+local accessory = brick.Accessory
+
+accessory.AttachmentPoint.Position = Vector3.new(0, 0.5, 1)
+accessory.AttachmentPoint.Rotation = Vector3.new(10, 20, 30)
