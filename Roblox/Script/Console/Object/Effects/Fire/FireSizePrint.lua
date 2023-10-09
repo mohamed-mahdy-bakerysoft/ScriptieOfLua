acc6403871c101c23859c37b1c2405343a39b515
EditaNEmilis <107065286+EditaNEmilis@users.noreply.github.com>
2023-10-09 19:49:47 +0300

    Console Object Effects script
    A way to print effects.

---
+++ Roblox/Script/Console/Object/Effects/Fire/FireSizePrint.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local fire = brick.Fire
+
+print(fire.Size)
---
+++ Roblox/Script/Console/Object/Effects/Highlight/HighlightFillColorPrint.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local highlight = brick.Highlight
+
+print(highlight.FillColor)
