d05ffec4704647abfa04bdf5a5c87f683b8d8bbe
EditaNEmilis <107065286+EditaNEmilis@users.noreply.github.com>
2023-10-12 17:49:38 +0300

    Object Uncategorized scripts
    Non-Classed Object type.

---
+++ Roblox/Script/Part/Object/Uncategorized/Configuration/Data/ConfigurationName.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local configuration = brick.Configuration
+
+configuration.Name = "Configuration"
---
+++ Roblox/Script/Part/Object/Uncategorized/Folder/Data/FolderName.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local folder = brick.Folder
+
+folder.Name = "Folder"
