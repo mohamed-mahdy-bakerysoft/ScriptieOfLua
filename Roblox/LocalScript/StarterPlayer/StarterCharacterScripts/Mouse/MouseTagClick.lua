055444241cde949cf3b3a4e74ed936191f9c87bd
EditaNEmilis <ramanauskiene.edita@gmail.com>
2023-10-31 22:45:42 +0200

    Mouse Tag Click script
    A script for detecting any tags. Might be a new one for this repo.

---
+++ Roblox/LocalScript/StarterPlayer/StarterCharacterScripts/Mouse/MouseTagClick.lua
@@ -0,0 +1,15 @@
+-- Locals
+local collection = game:GetService("CollectionService")
+local players = game.Players.LocalPlayer
+local mouse = players:GetMouse()
+
+-- Mouse Function
+mouse.Button1Down:Connect(function()
+	if not mouse.Target then
+		return nil
+	end
+	
+	if collection:HasTag(mouse.Target, "Click") then
+		print("Clicked!")
+	end
+end)
