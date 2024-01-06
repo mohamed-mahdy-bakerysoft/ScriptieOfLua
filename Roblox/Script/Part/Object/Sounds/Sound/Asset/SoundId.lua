6d96777f60b12314e63621684d2c36149a1ead5d
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-01-06 20:20:56 +0200

    Sound scripts
    Later for two sectors from this object next..

---
+++ Roblox/Script/Part/Object/Sounds/Sound/Asset/SoundId.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local sound = brick.Sound
+
+sound.SoundId = "rbxassetid://307631257"
---
+++ Roblox/Script/Part/Object/Sounds/Sound/Data/SoundClassName.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local sound = brick.Sound
+
+sound.ClassName = "Sound"
