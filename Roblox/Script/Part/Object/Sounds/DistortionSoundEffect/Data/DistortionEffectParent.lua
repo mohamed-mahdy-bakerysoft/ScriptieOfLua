85a75e5cb07ada0ee0db7559b88e42d0cd3f7900
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-01-12 19:01:50 +0200

    Distortion Sound Effect scripts
    A effect object for the main sound to manipulate the audio.

---
+++ Roblox/Script/Part/Object/Sounds/DistortionSoundEffect/Data/DistortionEffectParent.lua
@@ -0,0 +1,5 @@
+local brick = script.Parent
+local sound = brick.Sound
+local distortion = sound.DistortionSoundEffect
+
+distortion.Parent = workspace
---
+++ Roblox/Script/Part/Object/Sounds/DistortionSoundEffect/State/DistortionEffectLevel.lua
@@ -0,0 +1,5 @@
+local brick = script.Parent
+local sound = brick.Sound
+local distortion = sound.DistortionSoundEffect
+
+distortion.Level = 0.80
