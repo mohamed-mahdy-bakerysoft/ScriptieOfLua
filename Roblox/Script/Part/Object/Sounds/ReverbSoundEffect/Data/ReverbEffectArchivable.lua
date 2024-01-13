f4448368848fe1d70d031b5c4f3b1ccd62c2b356
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-01-13 16:50:13 +0200

    Reverb Sound Effect scripts
    Will make an echo effect to the sound's audio.

---
+++ Roblox/Script/Part/Object/Sounds/ReverbSoundEffect/Data/ReverbEffectArchivable.lua
@@ -0,0 +1,5 @@
+local brick = script.Parent
+local sound = brick.Sound
+local reverb = sound.ReverbSoundEffect
+
+reverb.Archivable = true
---
+++ Roblox/Script/Part/Object/Sounds/ReverbSoundEffect/State/ReverbDecayTime.lua
@@ -0,0 +1,5 @@
+local brick = script.Parent
+local sound = brick.Sound
+local reverb = sound.ReverbSoundEffect
+
+reverb.DecayTime = 1.50
