0e12772b508570fb7a12230a07919c404a1d0a45
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-01-19 20:10:43 +0200

    Echo Sound Effect scripts
    Probably almost the same thing as Reverb Sound Effect.. I guess.

---
+++ Roblox/Script/Part/Object/Sounds/EchoSoundEffect/Data/EchoEffectParent.lua
@@ -0,0 +1,5 @@
+local brick = script.Parent
+local sound = brick.Sound
+local echo = sound.EchoSoundEffect
+
+echo.Parent = workspace
---
+++ Roblox/Script/Part/Object/Sounds/EchoSoundEffect/State/EchoEffectDryLevel.lua
@@ -0,0 +1,5 @@
+local brick = script.Parent
+local sound = brick.Sound
+local echo = sound.EchoSoundEffect
+
+echo.DryLevel = 1.0
