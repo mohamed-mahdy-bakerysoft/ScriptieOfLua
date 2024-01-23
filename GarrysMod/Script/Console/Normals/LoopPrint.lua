0e63ee7bfd7c326ec06f6b5ad544193c9b718ab3
EditaNEmilis <107065286+EditaNEmilis@users.noreply.github.com>
2024-01-23 17:06:46 +0200

    Loop Print script
    Alternate way of looping than Roblox method..

---
+++ GarrysMod/Script/Console/Normals/LoopPrint.lua
@@ -0,0 +1,12 @@
+-- Local
+local delay = 0
+
+-- Add Hook to loop delay time
+hook.Add("Think", "CurTimeDelay", function()
+	if CurTime() < delay then 
+        return nil
+    end	
+
+	print("This message will repeat every 5 seconds.")
+	delay = CurTime() + 5
+end)
