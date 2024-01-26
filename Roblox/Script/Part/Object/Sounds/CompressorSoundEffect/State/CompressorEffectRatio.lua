fb690455c40832d834101392b2059af3946b3e3e
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-01-26 17:42:53 +0200

    Compressor Sound Effect scripts
    Last or another object sounds effect left.. Just hold on.

---
+++ Roblox/Script/Part/Object/Sounds/CompressorSoundEffect/State/CompressorEffectRatio.lua
@@ -0,0 +1,5 @@
+local brick = script.Parent
+local sound = brick.Sound
+local compressor = sound.CompressorSoundEffect
+
+compressor.Ratio = 40
---
+++ Roblox/Script/Part/Object/Sounds/CompressorSoundEffect/State/CompressorEffectRelease.lua
@@ -0,0 +1,5 @@
+local brick = script.Parent
+local sound = brick.Sound
+local compressor = sound.CompressorSoundEffect
+
+compressor.Release = 0.1
