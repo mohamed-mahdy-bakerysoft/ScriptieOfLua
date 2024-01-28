872a39309028a58c64210fd647438c12caf3a2a7
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-01-28 17:31:53 +0200

    Flange Sound Effect scripts
    I think that's all the sound effects now.. Hooray.

---
+++ Roblox/Script/Part/Object/Sounds/Effects/FlangeSoundEffect/Data/FlangeEffectClassName.lua
@@ -0,0 +1,5 @@
+local brick = script.Parent
+local sound = brick.Sound
+local flange = sound.FlangeSoundEffect
+
+flange.ClassName = "FlangeSoundEffect"
---
+++ Roblox/Script/Part/Object/Sounds/Effects/FlangeSoundEffect/State/FlangeEffectDepth.lua
@@ -0,0 +1,5 @@
+local brick = script.Parent
+local sound = brick.Sound
+local flange = sound.FlangeSoundEffect
+
+flange.Depth = 0.45
