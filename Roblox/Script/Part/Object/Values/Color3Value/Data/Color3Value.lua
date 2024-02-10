4b8023d5f043ed77d1c64e8f103689888be0864c
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-02-10 18:24:49 +0200

    Color3Value scripts
    For storing color3 float or something else.. Maybe.

---
+++ Roblox/Script/Part/Object/Values/Color3Value/Data/Color3Value.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local color3value = brick.Value
+
+color3value.Value = Color3.new(0.33, 0.66, 1)
---
+++ Roblox/Script/Part/Object/Values/Color3Value/Data/Color3ValueParent.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local color3value = brick.Value
+
+color3value.Parent = workspace
