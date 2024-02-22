42a4cb27bd6b349a63f00b904c6881ee71362bd3
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-02-22 17:59:11 +0200

    Vector3Value scripts + Mini-Fix
    Vector 3 value scripts and a mini fix from the String value.

---
+++ Roblox/Script/Part/Object/Values/Vector3Value/Vector3Value.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local vector3value = brick.Value
+
+vector3value.Value = Vector3.new(5, 10, 15)
---
+++ Roblox/Script/Part/Object/Values/Vector3Value/Vector3ValueClassName.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local vector3value = brick.Value
+
+vector3value.ClassName = "Vector3Value"
