498db88bf3a1d3ffff63970d7a3811536566ce28
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-03-10 23:02:00 +0200

    LocalScript scripts
    Same thing, nothing much.

---
+++ Roblox/Script/Part/Object/Scripting/LocalScript/Behavior/LocalScriptEnabled.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local localscript = brick.LocalScript
+
+localscript.Enabled = true
---
+++ Roblox/Script/Part/Object/Scripting/LocalScript/Data/LocalScriptParent.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local localscript = brick.LocalScript
+
+localscript.Parent = workspace
