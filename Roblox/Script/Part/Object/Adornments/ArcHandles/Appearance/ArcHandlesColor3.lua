e96359ae8c1cb81004615d4e6bc43178b71be6bb
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-03-24 20:45:31 +0200

    ArcHandles scripts
    An Arc Handle that you can show with Adornee.

---
+++ Roblox/Script/Part/Object/Adornments/ArcHandles/Appearance/ArcHandlesColor3.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local archandles = brick.ArcHandles
+
+archandles.Color3 = Color3.new(0.0509804, 0.411765, 0.67451)
---
+++ Roblox/Script/Part/Object/Adornments/ArcHandles/Data/ArcHandlesParent.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local archandles = brick.ArcHandles
+
+archandles.Parent = workspace
