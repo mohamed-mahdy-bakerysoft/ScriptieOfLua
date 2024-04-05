445aba1162a6d156be7745f1cbd4092cc00787eb
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-04-05 19:50:35 +0300

    RopeConstraint scripts update
    RopeConstraint updated with more script and features to it.

---
+++ Roblox/Script/Part/Object/Constraints/RopeConstraint/Winch/RopeWinchSpeed.lua
@@ -0,0 +1,13 @@
+local part = script.Parent
+
+local attachment0 = part.Attachment0
+local attachment1 = part.Attachment1
+
+local rope = part.RopeConstraint
+
+rope.Attachment0 = attachment0
+rope.Attachment1 = attachment1
+
+rope.WinchEnabled = true
+
+rope.WinchSpeed = 2
