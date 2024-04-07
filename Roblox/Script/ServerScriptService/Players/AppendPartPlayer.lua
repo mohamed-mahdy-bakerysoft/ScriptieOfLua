f602a703a677e0e0c2e3d45aaf4d7ed474fc9617
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-04-07 22:28:14 +0300

    Updated AppendPartPlayer
    Added Clone() function and CanCollide to false.

--- Roblox/Script/ServerScriptService/Players/AppendPartPlayer.lua
+++ Roblox/Script/ServerScriptService/Players/AppendPartPlayer.lua
@@ -7,7 +7,7 @@ local brick = serverstorage.Part
 -- Welder Function
 local function Welder(PartA, PartB, Parent)
 	local weldconstraint = Instance.new("WeldConstraint")
-	
+
 	weldconstraint.Part0 = PartA
 	weldconstraint.Part1 = PartB
 	weldconstraint.Parent = Parent
@@ -16,9 +16,11 @@ end
 -- Player Function
 players.PlayerAdded:Connect(function(plr:Player)
 	plr.CharacterAdded:Connect(function(char:Model)
-		Welder(brick, char.LeftHand, brick)
-		
-		brick.CFrame = (char.LeftHand.CFrame - Vector3.new(0, 0.15, 0))
-		brick.Parent = char.LeftHand
+		local brickclone = brick:Clone()
+		Welder(brickclone, char.LeftHand, brickclone)
+
+		brickclone.CFrame = (char.LeftHand.CFrame - Vector3.new(0, 0.15, 0))
+		brickclone.CanCollide = false
+		brickclone.Parent = char.LeftHand
 	end)
 end)
