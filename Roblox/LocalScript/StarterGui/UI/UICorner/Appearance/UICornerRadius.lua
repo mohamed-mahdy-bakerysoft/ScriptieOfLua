5e3da22e08a9aa6581319c218c958f1d91b71fd7
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-05-25 14:04:07 +0300

    UICorner scripts
    Might do more UI objects later.

---
+++ Roblox/LocalScript/StarterGui/UI/UICorner/Appearance/UICornerRadius.lua
@@ -0,0 +1,4 @@
+local gui = script.Parent
+local uicorner = gui.UICorner
+
+uicorner.CornerRadius = UDim.new(0, 10)
---
+++ Roblox/LocalScript/StarterGui/UI/UICorner/Data/UICornerClassName.lua
@@ -0,0 +1,4 @@
+local gui = script.Parent
+local uicorner = gui.UICorner
+
+uicorner.ClassName = "UICorner"
