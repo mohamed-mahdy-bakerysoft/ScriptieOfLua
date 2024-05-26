8b2fd250ee7a2fa0c7f89c3f5f756f2506e3bbad
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-05-26 14:32:23 +0300

    StarterGui update scripts
    Some folders name changes and other, and new ImageButton section.

---
+++ Roblox/LocalScript/StarterGui/Buttons/ImageButton/Data/BorderColorImageButton.lua
@@ -0,0 +1,5 @@
+local imagebutton = script.Parent.ImageButton
+
+imagebutton.MouseButton1Click:Connect(function()
+	imagebutton.BorderColor3 = Color3.new(1, 0.333333, 0.498039)
+end)
---
+++ Roblox/LocalScript/StarterGui/Buttons/TextButton/Data/BackgroundColorTextButton.lua
@@ -0,0 +1,5 @@
+local imagebutton = script.Parent.ImageButton
+
+imagebutton.MouseButton1Click:Connect(function()
+	imagebutton.BackgroundColor3 = Color3.new(1, 0.666667, 0.498039)
+end)
