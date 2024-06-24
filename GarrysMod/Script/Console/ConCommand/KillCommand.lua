827b2c72c97a86e93a97773c0d1d4947f0c5da03
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-06-24 15:46:28 +0300

    KillCommand script update
    Maybe not too simple for this section.. But hey, just an update.

--- GarrysMod/Script/Console/ConCommand/KillCommand.lua
+++ GarrysMod/Script/Console/ConCommand/KillCommand.lua
@@ -1,3 +1,6 @@
-local ent = Entity(1)
-
-ent:ConCommand("kill")
+-- A custom console command to kill the player
+concommand.Add("kill", function(ply)
+	if (ply:IsValid()) then
+		ply:ConCommand("kill")
+	end
+end)
