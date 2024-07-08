6369a9fc4937c209f0d0c66b4786c33767bddea7
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-07-08 22:59:25 +0300

    RemoteFunction script
    A script object for two-way communcation and such..

---
+++ Roblox/Script/Part/Object/Scripting/RemoteFunction/Data/RemoteFunctionArchivable.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local remotefunction = brick.RemoteFunction
+
+remotefunction.Archivable = true
---
+++ Roblox/Script/Part/Object/Scripting/RemoteFunction/Data/RemoteFunctionName.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local remotefunction = brick.RemoteFunction
+
+remotefunction.Name = "RemoteFunction"
