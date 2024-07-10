d7288ac1f9964caaaf846578cba764a762ea5d7a
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-07-10 23:43:36 +0300

    DragDetector Image scripts
    A folder for the 'Image' on DragDetector section.. Used for CursorIcon, not the best when comes to larger size.

---
+++ Roblox/Script/Part/Object/Interfaces/DragDetector/Image/DragDetectorActivatedCursorIcon.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local dragdetector = brick.DragDetector
+
+dragdetector.ActivatedCursorIcon = "rbxassetid://3203186317"
---
+++ Roblox/Script/Part/Object/Interfaces/DragDetector/Image/DragDetectorCursorIcon.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local dragdetector = brick.DragDetector
+
+dragdetector.CursorIcon = "rbxassetid://3203186317"
