64ce5170f076db793ada1c4d334463ef51999552
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-08-07 23:24:27 +0300

    WorldModel script
    Only object in this sector, I guess..

---
+++ Roblox/Script/Part/Object/World/WorldModel/Data/WorldModelArchivable.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local worldmodel = brick.WorldModel
+
+worldmodel.Archivable = true
---
+++ Roblox/Script/Part/Object/World/WorldModel/Pivot/WorldPivot/WorldModelOrientation.lua
@@ -0,0 +1,4 @@
+local brick = script.Parent
+local worldmodel = brick.WorldModel
+
+worldmodel.WorldPivot = CFrame.Angles(5, 10, 15)
