bb536b81c589cdabe911d780273862b624716c52
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-08-18 21:32:00 +0300

    NULL Print script
    An error value print script, specific to Garry's Mod lua that represents an invalid entity.

---
+++ GarrysMod/Script/Console/Normals/NULLPrint.lua
@@ -0,0 +1,5 @@
+print(NULL == NULL) -- True
+print(NULL == nil) -- False
+print(isentity(NULL)) -- True
+print(NULL.GetPos) -- Function: 0x81991d7a (Or something similar)
+print(NULL:GetPos()) -- [ERROR] XXX:XX: Tried to use a NULL entity! (Or something similar)
