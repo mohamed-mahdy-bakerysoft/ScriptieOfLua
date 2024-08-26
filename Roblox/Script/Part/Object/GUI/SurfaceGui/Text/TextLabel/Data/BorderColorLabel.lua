1dd52a432d53effc41315d79eec71da51a89cc75
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-08-26 18:08:44 +0300

    TextLabel update
    New folders and scripts for TextLabel

---
+++ Roblox/Script/Part/Object/GUI/SurfaceGui/Text/TextLabel/Data/BorderColorLabel.lua
@@ -0,0 +1,4 @@
+local surfacegui = script.Parent.SurfaceGui
+local textlabel = surfacegui.TextLabel
+
+textlabel.BorderColor3 = Color3.new(1, 0.666667, 0)
---
+++ Roblox/Script/Part/Object/GUI/SurfaceGui/Text/TextLabel/Data/BorderSizePixelLabel.lua
@@ -0,0 +1,4 @@
+local surfacegui = script.Parent.SurfaceGui
+local textlabel = surfacegui.TextLabel
+
+textlabel.BorderSizePixel = 10
