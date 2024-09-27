ae086eefce108a6afff3a2a35476dfd70208081b
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-09-27 18:20:19 +0300

    Angle print scripts
    For server and client.

---
+++ GarrysMod/LocalScript/Console/Globals/AnglePrint.lua
@@ -0,0 +1,10 @@
+print(Angle(1, 2, 3)) -- Interger
+print(Angle("4 5 6")) -- Strings
+
+-- Local
+local test = Angle(7, 8, 9)
+print(Angle(test))
+
+-- Doesn't work
+print(Angle("4 5 test"))
+print(Angle())
