cca73120fd3ed05722e4e3c857eeef4b074676d0
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-10-01 16:25:55 +0300

    GetOptionText Panel script
    Option choice text script.

---
+++ GarrysMod/LocalScript/Panels/GetOptionTextPanel.lua
@@ -0,0 +1,14 @@
+-- CBox
+local cbox = vgui.Create("DComboBox")
+cbox:SetPos(5, 5)
+cbox:SetSize(200, 20)
+
+-- Values and Choice
+cbox:SetValue("Colors")
+cbox:AddChoice("Red")
+cbox:AddChoice("Green")
+cbox:AddChoice("Blue")
+cbox:AddChoice("Yellow")
+
+-- Print
+print(cbox:GetOptionText(3))
